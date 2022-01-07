39edb0d8f8e404423fa09d52341af53369e9df2e
Kenny Yoon <ghun.yoon@gmail.com>
2022-01-07 18:54:55 -0500

    create sample model, user

--- apptwo/models.py
+++ apptwo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
+class SampleApp(models.Model):
+    title = models.TextField()
+    quantity = models.IntegerField(default=1)
+
 # Create your models here.
---
+++ test.py
@@ -0,0 +1,9 @@
+class Yo():
+    myName = "Kenny"
+
+    def __str__(self):
+        return self.myName + '22'
+
+
+obj = Yo()
+print(obj)
