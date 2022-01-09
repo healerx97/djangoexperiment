52416ebc2d54e097abba2702a8d5614e265b6b5e
Kenny Yoon <ghun.yoon@gmail.com>
2022-01-08 22:57:43 -0500

    adding migrations

--- apptwo/models.py
+++ apptwo/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class SampleApp(models.Model):
     title = models.TextField()
     quantity = models.IntegerField(default=1)
-
+    extra = models.BooleanField(null = True)
 # Create your models here.
