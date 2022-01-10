1773571ce7b793750c9e64d275a160f56b448878
Kenny Yoon <ghun.yoon@gmail.com>
2022-01-09 22:55:19 -0500

    admin

--- apptwo/admin.py
+++ apptwo/admin.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from .models import SampleApp
+from .models import SampleModelTwo
 
-admin.site.register(SampleApp)
+# admin.site.unregister(SampleApp)
+# admin.site.register(SampleModelTwo)
+
+
+from django.apps import apps
+
+models = apps.get_models()
+
+for model in models:
+    try:
+        admin.site.register(model)
+    except admin.sites.AlreadyRegistered:
+        pass
 # Register your models here.
--- apptwo/models.py
+++ apptwo/models.py
@@ -4,4 +4,7 @@ class SampleApp(models.Model):
     title = models.TextField()
     quantity = models.IntegerField(default=1)
     extra = models.BooleanField(null = True)
+
+class SampleModelTwo(models.Model):
+    name = models.TextField()
 # Create your models here.
