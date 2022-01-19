ee818c8ce28426caa66b458982c3788afc63104d
Kenny Yoon <ghun.yoon@gmail.com>
2022-01-19 11:59:33 -0500

    test2

--- test.py
+++ test.py
@@ -1,10 +1,3 @@
-class Yo():
-    myName = "Kenny"
-
-    def __str__(self):
-        return self.myName + '22'
-
-
-obj = Yo()
-print(obj)
-print(obj.myName)
+class Test2:
+    def __init__(self):
+        self.my = "test2"
