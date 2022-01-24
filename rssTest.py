9227a749003397631b86d1dd2f37f1bf12ad153b
Kenny Yoon <ghun.yoon@gmail.com>
2022-01-23 21:35:44 -0500

    rss csv

--- rssTest.py
+++ rssTest.py
@@ -1,5 +1,5 @@
 import feedparser
-
+import csv
 parsedFeed = feedparser.parse("https://techcrunch.com/feed/")
 
 entry = parsedFeed.entries[2]
@@ -7,5 +7,14 @@ print(entry.keys())
 
 
 
-# for entry in parsedFeed.entries:
-#     print(entry['content'])
+with open('rssFeed.csv', 'w') as file:
+    f = csv.writer(file)
+    f.writerow(['Title', 'Summary', 'Published Date'])
+    for entry in parsedFeed.entries:
+        s = [entry['title'], entry['summary'], entry['published']]
+        f.writerow(s)
+
+
+
+
+# keys = ['title', 'title_detail', 'links', 'link', 'comments', 'authors', 'author', 'author_detail', 'published', 'published_parsed', 'tags', 'id', 'guidislink', 'summary', 'summary_detail', 'content', 'wfw_commentrss', 'slash_comments', 'post-id']
